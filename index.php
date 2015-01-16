ca67e8f293b6ec34d159a2d398742f683bb2c07f
peter <peter@peter-OptiPlex-755>
2015-01-17 00:15:39 +0100

    Edit software name

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php
 /*~ index.php
 .---------------------------------------------------------------------------.
-|  Software: SencilloCache                                                  |
-|   Version: 2014.005                                                       |
+|  Software: Sencillo Index                                                 |
+|   Version: 2015.003                                                       |
 |   Contact: ph@mastery.sk                                                  |
 | ------------------------------------------------------------------------- |
 |    Author: Bc. Peter Horváth (original founder)                           |
-| Copyright (c) 2014, Bc. Peter Horváth. All Rights Reserved.               |
+| Copyright (c) 2015, Bc. Peter Horváth. All Rights Reserved.               |
 | ------------------------------------------------------------------------- |
 |   License: Distributed under the General Public License (GPL)             |
 |            http://www.gnu.org/copyleft/gpl.html                           |
