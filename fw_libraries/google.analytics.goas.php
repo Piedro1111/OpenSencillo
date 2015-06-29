599588fcf65e11e2e8ee9ac65047480e32348b89
Piedro1111 <info@opensencillo.com>
2015-06-29 23:35:37 +0200

    Update system core

--- fw_libraries/google.analytics.goas.php
+++ fw_libraries/google.analytics.goas.php
@@ -1,4 +1,13 @@
 <?php
+/**
+ * Google Analytics for OpenSencillo
+ * @name GOAS
+ * @version 2015.005
+ * @category Sencillo Library
+ * @see http://www.opensencillo.com
+ * @author Bc. Peter Horváth
+ * @license Distributed under the General Public License (GPL) http://www.gnu.org/licenses/gpl-3.0.html This program is distributed in the hope that it will be useful - WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ */
 class analytics
 {
 	protected $arr;
@@ -10,10 +19,14 @@ class analytics
 		$this->arr[] = "(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),";
 		$this->arr[] = "m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)";
 		$this->arr[] = "})(window,document,'script','//www.google-analytics.com/analytics.js','ga');";
-		$this->gadefault('UA-50042410-7');
-		$this->create();
 	}
 	
+	/**
+	 * Add custom universal analytics
+	 * @param $pref string (create|send|require)
+	 * @param $value string (UA-XXXX-Y|pageview|...)
+	 * @param $param string (auto|...)
+	 */
 	public function set($pref,$value,$param)
 	{
 		if(!empty($param))
@@ -26,16 +39,24 @@ class analytics
 		}
 	}
 	
+	/**
+	 * Set GA to default basic options
+	 * @param $id string (UA-XXXX-Y)
+	 */
 	public function gadefault($id)
 	{
 		$this->arr[] = "ga('create', '$id', 'auto');";
 		$this->arr[] = "ga('send', 'pageview');";
 	}
 	
+	/**
+	 * Create Google Analytics Code
+	 */
 	public function create()
 	{
 		$this->arr[] = "</script>";
 		$this->arr = implode(PHP_EOL,$this->arr);
+		return $this->arr;
 	}
 }
 ?>
