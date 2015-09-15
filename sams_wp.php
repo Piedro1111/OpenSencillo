abb5a1ac75035d491497672544d46a654e2335a4
Piedro1111 <info@opensencillo.com>
2015-09-15 02:43:23 +0200

    edit SAMS prepare for WP

--- sams_wp.php
+++ sams_wp.php
@@ -1,11 +1,12 @@
 <?php
 /*
 Plugin Name: OpenSencillo SAMS
-Plugin URI: http://www.mastery.sk/
+Plugin URI: http://www.opensencillo.com/
 Description: A OpenSencillo SAMS for WP.
 Version: 2015.109
 Author: Bc. Peter Horváth
 Author URI: http://phorvath.com
 License: GPL3
 */
+defined( 'ABSPATH' ) or die( 'Plugin file cannot be accessed directly.' );
 ?>
