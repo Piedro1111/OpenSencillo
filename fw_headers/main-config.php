e8cf29e5f3eff75e797c5195201928f99a8d9965
Bc. Peter Horváth <info@opensencillo.com>
2015-10-12 00:09:23 +0200

    Update main-config.php

--- fw_headers/main-config.php
+++ fw_headers/main-config.php
@@ -10,15 +10,9 @@
  * @author Bc. Peter Horváth
  * @license Distributed under the General Public License (GPL) http://www.gnu.org/licenses/gpl-3.0.html This program is distributed in the hope that it will be useful - WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  */
-class startInfo
-{
-	const PAGE = $_GET['p'];
-	const USER_IP = $_SERVER["REMOTE_ADDR"];
-	const USER_BROWSER = $_SERVER["HTTP_USER_AGENT"];
-}
-define('PAGE',startInfo::PAGE);
-define("USER_IP",startInfo::USER_IP);
-define("USER_BROWSER",startInfo::USER_BROWSER);
+define('PAGE',$_GET["p"]);
+define("USER_IP",$_SERVER["REMOTE_ADDR"]);
+define("USER_BROWSER",$_SERVER["HTTP_USER_AGENT"]);
 if(isset($_SERVER['HTTPS']) && ($_SERVER['HTTPS'] == 'on' || $_SERVER['HTTPS'] == 1) || isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https')
 {
 	$protocol = 'https://';
