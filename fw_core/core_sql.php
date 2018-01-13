e04f625793553796b8a233042ac6e3c96e4fc543
Bc. Peter Horváth <info@opensencillo.com>
2018-01-13 01:17:45 +0100

    Update core_sql.php

--- fw_core/core_sql.php
+++ fw_core/core_sql.php
@@ -551,7 +551,10 @@ class mysqlInterface extends mysqlEdit
 						$data_condition.=' BETWEEN '.$key_att.' AND '.$val_att;
 					break;
 					case 'set':
-						$data_set.=$key_att.'='.$val_att.',';
+						foreach($val_col as $key_att=>$val_att)
+						{
+							$data_set.=$key_att.'='.((is_string($val_att))?"'{$val_att}'":$val_att).',';
+						}
 					break;
 					case 'sort':
 						$data_sort=' ORDER BY ';
